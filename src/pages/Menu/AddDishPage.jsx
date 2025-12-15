c438c0d88e7ace407812afcf0bf13616b29814a2
shivamkumar00001 <shivamkumar933401@gmail.com>
2025-12-15 08:33:13 +0530

    updated

--- src/pages/Menu/AddDishPage.jsx
+++ src/pages/Menu/AddDishPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import axiosClient from "../../api/AxiosClient";
+import axiosClient from "../../api/axiosClient";
 
 function makeSlug(text = "") {
   return text
